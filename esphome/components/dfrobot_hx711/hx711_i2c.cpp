4bbaaf044a660c8a3ea57b47b6fd17efcca1c5b4
Sejl <sejlau@gmail.com>
2024-07-05 10:17:45 +0000

    First commit. Basic definitions.

---
+++ esphome/components/dfrobot_hx711/hx711_i2c.cpp
@@ -0,0 +1,20 @@
+#include "hx711_i2c.h"
+#include "esphome/core/log.h"
+#include "esphome/core/hal.h"
+
+// Based on:
+// https://github.com/DFRobot/DFRobot_HX711_I2C
+// https://www.dfrobot.com/product-2289.html
+// https://wiki.dfrobot.com/HX711_Weight_Sensor_Kit_SKU_KIT0176
+
+
+
+namespace esphome {
+namespace hx711_i2c {
+
+static const char *const TAG = "hx711_i2c";
+
+
+
+} // namespace hx711_i2c
+} // namespace esphome
---
+++ esphome/components/dfrobot_hx711/hx711_i2c.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/i2c/i2c.h"
+
+#include <vector>
+
+namespace esphome {
+namespace hx711_i2c {
+
+
+
+
+} // namespace hx711_i2c
+} // namespace esphome
